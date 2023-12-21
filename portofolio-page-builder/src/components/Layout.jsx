7a6d86a54f59a61ca8056efab5b09c5d444b3af3
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-12-21 14:15:29 +0700

    add: create modal and refactor layout

--- portofolio-page-builder/src/components/Layout.jsx
+++ portofolio-page-builder/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout = ({ children }) => {
   return (
-    <section className="flex flex-col w-screen h-max justify-center items-center">
+    <section className="flex flex-col w-full h-max justify-center items-center">
       {children}
     </section>
   );
---
+++ portofolio-page-builder/src/components/Modal.jsx
@@ -0,0 +1,22 @@
+const Modal = ({ isOpen, onClose, children }) => {
+  const handleBackgroundClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center"
+          onClick={handleBackgroundClick}
+        >
+          <div className="bg-white p-4 rounded-md shadow-md">{children}</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
