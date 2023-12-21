80a9c68eaf65c2f379ae13ae150b5b4aab031050
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-12-21 09:10:16 +0700

    add: create landing page

--- portofolio-page-builder/src/pages/landing/index.jsx
+++ portofolio-page-builder/src/pages/landing/index.jsx
@@ -1,5 +1,15 @@
+import Layout from "../../components/Layout";
+import Navbar from "../../components/Navbar";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Layout>
+      <div className="fixed top-0">
+        <Navbar />
+      </div>
+      <div>Landing</div>
+    </Layout>
+  );
 };
 
 export default Landing;
